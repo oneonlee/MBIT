be768ad5c464055d72dda516070565ce69932e81
Lee Dong-geon <peter8739@gmail.com>
2021-06-22 13:56:45 +0900

    2021. 06. 22. backend

---
+++ backend/main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('form/', views.form, name='form'),
+    path('submit/', views.submit, name='submit'),
+    path('result/<int:developer_id>', views.result, name='result'),
+    path('all_results', views.all_results, name='all_results'),
+    
+]
---
+++ backend/main/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from .models import Question, Developer, Choice
+
+def index(request):
+    developers = Developer.objects.all()
+    
+    context = {
+        'developers' : developers,
+    }
+    
+    return render(request, 'main/index.html', context = context)
+
+def form(request):
+    questions = Question.objects.all()
+    
+    context = {
+        'questions' : questions,
+    }
+    
+    return render(request, 'main/form.html', context = context)
+
+def submit(request):
+    # 전체 문항 수 : N
+    N= Question.objects.count()
+    
+    # 개발자 유형 수 : K
+    K = Developer.objects.count()
+    
+    counter = [0] * (K + 1)
+    
+    for n in range (1, N+1):
+        developer_id = int(request.POST[f'question-{n}'][0])
+        counter[developer_id] += 1
+        
+    # 최고점 개발 유형
+    best_developer_id = max(range(1, K+1), key=lambda id: counter[id])
+    best_developer = Developer.objects.get(pk = best_developer_id)
+    best_developer.count += 1
+    best_developer.save()
+    
+    context = {
+        'developer' : best_developer,
+        'counter' : counter
+    }
+    
+    
+    return redirect('main:result', developer_id=best_developer_id)
+
+def result(request, developer_id):
+    developer = Developer.objects.get(pk=developer_id)
+    context = {
+        'developer' : developer,
+    }
+    return render(request, 'main/result.html', context=context)
+
+def all_results(request):
+    return render(request, 'main/all_results.html')
