03b75b388ede46d95f8c19ce119570e77130e35d
Lee Dong-geon <peter8739@gmail.com>
2021-06-22 14:50:01 +0900

    2021. 06. 22. backend

--- backend/staticfiles/js/form.js
+++ backend/staticfiles/js/form.js
@@ -1,44 +1,33 @@
-function scrollUp() {
-    const vheight = $('.test').height(); // 페이지 고유의 높이값을 상수로 지정해서 사용 
+function scrollUp(top) {
+    const vheight = $('.test').height();
+    const margin_top = parseInt($('#survey').css('margin-top'), 10);
     $('html, body').animate({
-      scrollTop: ((Math.ceil($(window).scrollTop() / vheight)-1) * vheight)
+        scrollTop: top - vheight - margin_top
     }, 500);
 };
 
-function scrollDown() {
+function scrollDown(top) {
     const vheight = $('.test').height();
+    const margin_top = parseInt($('#survey').css('margin-top'), 10);
     $('html, body').animate({
-        scrollTop: ((Math.floor($(window).scrollTop() / vheight)+1) * vheight)
+        scrollTop: vheight + top - margin_top
     }, 500);
 }
 
-$(function(){
-    $('.next_btn').click(function(e){
-        let divs = $(this).parent().prev().children();
-        let inputs = divs.find('input:checked');
-        if(inputs.length < 1) {
-            alert('문항이 선택되지 않았습니다.');
-            return false;
-        }
-        e.preventDefault();
-        scrollDown();
-    });
+$('.next_btn').click(function(e){
+    let divs = $(this).parent().prev().children();
+    let present_top = $(this).parent().parent()[0].offsetTop;
+    let inputs = divs.find('input:checked');
+    if(inputs.length < 1) {
+        alert('문항이 선택되지 않았습니다.');
+        return false;
+    }
+    e.preventDefault();
+    scrollDown(present_top);
+});
 
-    $('.prev_btn').click(function(e){
-        e.preventDefault();
-        scrollUp();
-    });
-
-    $("#form").submit(function() {
-        let radios = $('input[type=radio]:checked');
-        if(radios.length < 10) {
-            alert("문항이 선택되지 않았습니다.");
-            return false;
-        }
-        return true;
-    });
-
-    $("html, body").animate({
-        scrollTop: 0
-    }, 500);
+$('.prev_btn').click(function(e){
+    let present_top = $(this).parent().parent()[0].offsetTop;
+    e.preventDefault();
+    scrollUp(present_top);
 });
